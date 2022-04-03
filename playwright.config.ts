2f54d692758fd26d28e47497ca49e064913afffc
Michael Nelson <revnelson@gmail.com>
2022-04-04 00:08:45 +0800

    Add testing functionality.
    Install and configure basic testing functionality.
    
    - Install Jest + React Testing Library and Playwright
    - Add test script to package.json that scans __tests__ folder
    - Add test:e2e script to package.json that scans __e2e__ folder
    - Add boilerplate tests
    
    Signed-off-by: Michael Nelson <revnelson@gmail.com>

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import { PlaywrightTestConfig } from "@playwright/test";
+const config: PlaywrightTestConfig = {
+  use: {
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: "on-first-retry",
+    baseURL: "http://localhost:4281",
+  },
+  projects: [
+    {
+      name: "ARTillery Design",
+      testDir: "./__e2e__",
+    },
+  ],
+};
+export default config;
