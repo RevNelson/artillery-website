94e1f63fd19c4b6174657b40239587b39fe4e5c8
Michael Nelson <revnelson@gmail.com>
2022-04-13 21:18:29 +0800

    Add ShoppingCart component.
    - Create Modal component
    - Create Image component
    - (api) Create products fragments
    - Create useCart hook
    - Create ShoppingCart component and styled component.
    - Add ShoppingCart component to UserNav
    
    Signed-off-by: Michael Nelson <revnelson@gmail.com>

--- components/Layout/Header/UserNav/UserNav.tsx
+++ components/Layout/Header/UserNav/UserNav.tsx
@@ -1,7 +1,6 @@
-import ShoppingBagIcon from "@heroicons/react/outline/ShoppingBagIcon"
-
 import ProfileMenu from "./ProfileMenu"
 import LanguageMenu from "./LanguageMenu"
+import ShoppingCart from "./ShoppingCart"
 
 // ####
 // #### Component
@@ -21,14 +20,7 @@ const UserNav = () => {
 
           {/* Cart */}
           <div className="ml-4 flow-root lg:ml-8">
-            <a href="#" className="group -m-2 p-2 flex items-center">
-              <ShoppingBagIcon
-                className="flex-shrink-0 h-6 w-6 text-white"
-                aria-hidden="true"
-              />
-              <span className="ml-2 text-sm font-medium text-white">0</span>
-              <span className="sr-only">items in cart, view bag</span>
-            </a>
+            <ShoppingCart />
           </div>
         </div>
       </div>
---
+++ components/ui/Modal/Modal.tsx
@@ -0,0 +1,81 @@
+import {
+  Children,
+  cloneElement,
+  FC,
+  Fragment,
+  ReactElement,
+  useRef,
+} from "react"
+import { Dialog, Transition } from "@headlessui/react"
+
+// ####
+// #### Types
+// ####
+
+type PropsType = {
+  open: boolean
+  setOpen: (value: boolean) => void
+  children: ReactElement
+}
+
+// ####
+// #### Component
+// ####
+
+const Modal: FC<PropsType> = ({ open, setOpen, children }) => {
+  const firstFocusRef = useRef(null)
+  return (
+    <Transition appear show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-40 overflow-y-auto"
+        initialFocus={firstFocusRef}
+        onClose={() => setOpen(false)}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="duration-200 ease-in"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+              {/* Dialog Content Goes Here */}
+              {Children.map(children, child => {
+                return cloneElement(child, {
+                  modalRef: firstFocusRef,
+                  setOpen,
+                })
+              })}
+              {/* Dialog Content Ends Here */}
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  )
+}
+
+export default Modal
